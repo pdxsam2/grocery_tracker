aac1f698210336b0224f9954c3a8233cec38e1b4
Samuel Parker <samuel.j.parker@outlook.com>
2023-08-11 23:21:38 -0700

    Added groceryTable.txt for storing all grocery items Added main loop for collecting grocery item indeces Moved credentials collection to its own function

--- grocery_tracker.py
+++ grocery_tracker.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import os.path
 import json
+import csv
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -9,15 +10,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-
-def main():
-    with open('docs_info.json', 'r') as file:
-        doc_info = json.load(file)
-
-    docId = doc_info['documentId']
-    scope = doc_info['scope']
+def getCredentials(docId, scope):
     creds = None
-    print(docId)
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', scope)
     if not creds or not creds.valid:
@@ -30,28 +24,50 @@ def main():
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
+    return creds
+
+def main():
+    with open('documentInfo.json', 'r') as file:
+        documentInfo = json.load(file)
+    scope = documentInfo['scope']
+    docId = documentInfo['documentId']
+    creds = getCredentials(docId, scope)
+    groceryTable = open('groceryTable.txt', 'r').read().split()
+    maxGroceryIndex = len(groceryTable) - 1
+    requests = [
+        {
+            'insertText': {
+                'location': {
+                    'index': 39
+                },
+                'text': ''
+            }
+        }
+    ]
+
 
     try:
         service = build('docs', 'v1', credentials=creds)
         document = service.documents().get(documentId=docId).execute()
 
-        requests = [
-            {
-                'insertText': {
-                    'location': {
-                        'index': 25
-                    },
-                    'text': 'onions'
-                }
-            }
-        ]
+        while 1:
+            print('enter your index:')
+            groceryIndex = int(input())
+            groceryIndex -= 1
 
-        result = service.documents().batchUpdate(
-            documentId=docId, body={'requests': requests}).execute()
+            if groceryIndex > maxGroceryIndex:
+                print("grocery index out of bounds")
+                continue
+            
+            groceryItem = groceryTable[groceryIndex]
+            requests[0]['insertText']['text'] = groceryItem + '\n'
+            service.documents().batchUpdate(
+                documentId=docId, 
+                body={'requests': requests}
+            ).execute()
 
     except HttpError as err:
         print(err)
-
 
 if __name__ == '__main__':
     main()
